569f6a07881c49eb54a74bf4bcdc3fe22ca8afa9
Gibso <oliver.goertz@gmail.com>
2019-09-06 15:44:23 +0200

    remove duplicate code

--- app/controllers/input-specifier.js
+++ app/controllers/input-specifier.js
@@ -1,15 +1,10 @@
 import ConceptSpecifierController from './concept-specifier'
 import { computed } from '@ember/object';
-import { formatSpec2Html } from "./utils/spec-formatter";
 
 export default ConceptSpecifierController.extend({
 
   concept1: null,
   concept2: null,
-
-  spec: computed('model', function () {
-    return formatSpec2Html(this.model.spec)
-  }),
 
   paramsAreValid: computed('concept1', 'concept2', function () {
     return !!this.concept1 && !!this.concept2;
